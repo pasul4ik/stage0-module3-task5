938196a88235817610b2938de82d40883ea5f58c
PavelDenisov <pavel.den111@gmail.com>
2023-02-13 17:01:53 +0200

    update 3

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class ThreeStuckVars {
     public static void main(String[] args) {
 
-        int first = 1, second = 10, third = 100;
-        System.out.println(first+second+third);
+        int first = 1;
+        int second = 10;
+        int third = 100;
+        System.out.println(first + " " + second+" " + third);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -5,11 +5,7 @@ import java.util.Scanner;
 public class TimeConvertor {
     public void convert(float minutes) {
 
-        Scanner S = new Scanner(System.in);
-        int seconds = 60;
-        System.out.println("please enter the number of minutes you want to convert?");
-        int minuts = S.nextInt();
-        int answer = minuts * seconds;
-        System.out.println(answer);
+        float seconds = minutes * 60;
+        System.out.println(seconds);
     }
 }
