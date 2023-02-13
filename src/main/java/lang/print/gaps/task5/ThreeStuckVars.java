898cd9abed1b9441c5deb7caae473a5861f95dc6
PavelDenisov <pavel.den111@gmail.com>
2023-02-13 17:08:59 +0200

    update 4

--- src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
+++ src/main/java/lang/print/gaps/task5/ThreeStuckVars.java
@@ -5,9 +5,8 @@ import java.util.Scanner;
 public class ThreeStuckVars {
     public static void main(String[] args) {
 
-        int first = 1;
-        int second = 10;
-        int third = 100;
+        int first = 1, second = 10, third = 100;
+
         System.out.println(first + " " + second+" " + third);
     }
 }
